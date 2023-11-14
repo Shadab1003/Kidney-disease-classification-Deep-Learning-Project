206ce80a9f9ea07f26d945b28e143f6d57f6f9fb
Shadab Ahmed <ahmed11shadab@gmail.com>
2023-11-14 22:21:00 +0530

    Model evaluation added

--- src/CNN_Classifier/config/configuration.py
+++ src/CNN_Classifier/config/configuration.py
@@ -1,9 +1,10 @@
 import os
 from src.CNN_Classifier.constants import *
-from src.CNN_Classifier.utils.common import read_yaml, create_directories,save_json
+from src.CNN_Classifier.utils.common import read_yaml, create_directories, save_json
 from src.CNN_Classifier.entity.config_entity import (DataIngestionConfig,
                                                      PrepareBaseModelConfig,
-                                                     TrainingConfig)
+                                                     TrainingConfig,
+                                                     EvaluationConfig)
 
 
 class ConfigurationManager:
@@ -72,6 +73,17 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney-ct-scan-image",
+            mlflow_uri="https://dagshub.com/ahmed11shadab/Kidney-disease-classification-Deep-Learning-Project.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
 
 
     
